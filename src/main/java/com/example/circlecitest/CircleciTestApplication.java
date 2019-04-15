b7349cfdf0b2b27a5d7b68abbdeed99e76a9472f
Ted Wei <ted.wei@opennet.tw>
2019-04-15 14:07:56 +0800

    First commit

---
+++ src/main/java/com/example/circlecitest/CircleciTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.circlecitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleciTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircleciTestApplication.class, args);
+	}
+
+}
